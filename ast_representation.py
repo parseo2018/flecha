6c6d0489d90459f055bcd156a8245cd22cfc1984
ruizdiazchristiand <ruizdiazchristiand@gmail.com>
2018-12-10 00:58:18 -0300

    Test de anidamiento y ajustes de printeo

--- ast_representation.py
+++ ast_representation.py
@@ -74,9 +74,9 @@ class CaseBranch(Node):
 		ret = " "*level+ "[\"" +str(self.typeNode) + "\" , "
 		ret += "\"" + self.children[0] + "\", ["
 		if self.children[1]:
-			ret += self.children[1][0]
+			ret += "\""+self.children[1][0]+"\""
 			for child in self.children[1][1:]:
-				ret += "," + child
+				ret += ", " + "\""+child+"\""
 		ret += "],\n"
 		ret += self.children[2].__str__(level+1) + "\n"
 		ret += " "*level+ "]"
@@ -121,7 +121,6 @@ class LambdaExpression(Node):
 			children[0] = children[0][1:]
 			Node.__init__(self, 'ExprLambda', [next_param, LambdaExpression(children=children)], leaf)
 		elif count_params == 1:
-			print(children)
 			Node.__init__(self, 'ExprLambda', [children[0][0], children[1]], leaf)
 
 	def __str__(self, level=0):
@@ -137,7 +136,7 @@ class LetExpression(Node):
 	def __str__(self, level=0):
 		ret = " "*level+ "[\"" +str(self.typeNode)+", \""+self.children[0]+"\",\n"
 		if self.children[1]:
-			ret += self.children[1].__str__(level+1) + "\n"
+			ret += self.children[1].__str__(level+1) + ",\n"
 		ret += self.children[2].__str__(level+1) + "\n"
 		ret += " "*level+ "]"
 		return ret
