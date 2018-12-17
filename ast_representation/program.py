579fb5b91a71ec19a0584c510068762473b916b8
ruizdiazchristiand <ruizdiazchristiand@gmail.com>
2018-12-17 03:25:52 -0300

    Fix en el print de un programa vacío

--- ast_representation/program.py
+++ ast_representation/program.py
@@ -11,3 +11,14 @@ class Program(Node):
 	def push(self,child):
 		self.children = self.children + [child]
 		return self
+
+	def __str__(self, level=0):
+		ret = " "*level+ "["
+		if self.children:
+			ret += "\n" + self.children[0].__str__(level+1)
+			for child in self.children[1:]:
+				ret += ",\n" + child.__str__(level+1)
+			ret += " "*level+ "\n]"
+		else:
+			ret += " "*level+ "]"
+		return ret
